9cb4b48d025c58efb150840ccd784660782b0230
BalakinMike <mikb2220@gmail.com>
2022-10-19 14:42:20 +0300

    финальный релиз на сдачу итога + примерный файл от руководителей

--- hw9.0.py
+++ hw9.0.py
@@ -1,11 +1,11 @@
-from os import read
-import requests, json
-from urllib.parse import urlencode
-#from token_my import TOKEN
+import requests, json, os
+# from urllib.parse import urlencode
 
 
 APP_ID = 51442805
-TOKEN = 'vk1.a.Bv85NseNvhbO1LmqMjyWHBNB-OL4tCYDOdjHhvvtTAQ-5rVEMThWpndE-wF2xTEhI6VE_-wNOJRtGwkIT8jbVb6uisDut3Iiilj-x2BqnbfitGoauAv-jtMvB2gyXbu-Bw3iQGCt7Gvq95aGSKeSMnCC-jNcdF3ZPqc_h-RPsxUw3DvMy5Qodp_6_Pu72XKEklSVuuHXhoLggaeqxnh_Gw'
+TOKEN_VK = 'vk1.a.Bv85NseNvhbO1LmqMjyWHBNB-OL4tCYDOdjHhvvtTAQ-5rVEMThWpndE-wF2xTEhI6VE_-wNOJRtGwkIT8jbVb6uisDut3Iiilj-x2BqnbfitGoauAv-jtMvB2gyXbu-Bw3iQGCt7Gvq95aGSKeSMnCC-jNcdF3ZPqc_h-RPsxUw3DvMy5Qodp_6_Pu72XKEklSVuuHXhoLggaeqxnh_Gw'
+TOKEN_Ya = 'y0_AgAAAAAMU5B4AADLWwAAAADQXDXFdBHPAWnrSai5pHFFKdIWfWAhyNQ'
+BASE_DIR = os.getcwd()
 
 class VKClient:
     BASE_URL: str = "https://api.vk.com/method/"
@@ -16,7 +16,7 @@ class VKClient:
     
 
     def __init__(self, TOKEN: str = None, user_id: str = "1"):
-        self.token = TOKEN
+        self.token = TOKEN_VK
         self.user_id = user_id
 
     
@@ -29,17 +29,66 @@ class VKClient:
             "access_token": self.token,
             "v": self.PROTOCOL_VERSION,
             "album_id": "wall",
+            "offset": "1",
+            "count": "1"
             
         }
 
         response = requests.get(url, params=params)
-        with open(r'final_file.json', 'w') as file_write:
+        with open(r'foto_list.json', 'w') as file_write:
             json.dump(response.json(), file_write)
-                   
-        return response.json()
+
+        name_file = 'foto_list.json'           
+        return name_file
+
+class YandexDisk:
+    URL_UPLOAD_LINK: str = "https://cloud-api.yandex.net/v1/disk/resources/upload"
 
 
-client = VKClient(TOKEN, "9214619")
-result = client.get_photos()
-# print(result)
+    def __init__(self, token: str):
+        self.token = token
+
+    @property
+    def header(self):
+        return {
+            "Content-Type": "application/json",
+            "Authorization": f"OAuth {self.token}"
+        }
+
+    def _get_upload_link(self, ya_disk_path: str):
+        params = {"path": ya_disk_path, "overwrite": "true"}
+        response = requests.get(self.URL_UPLOAD_LINK, headers=self.header, params=params)
+        
+        if response.status_code != 200:
+            raise requests.exceptions.RequestException
+        
+        upload_url = response.json().get("href")
+        return upload_url
+
+    def uploader(self, ya_disk_path: str, file_path: str):
+        upload_link = self._get_upload_link(ya_disk_path)
+        with open(file_path, 'rb') as file_obj:
+            response = requests.put(upload_link, data=file_obj)
+            if response.status_code == 201:
+                print('Успешно загрузили')
+            elif response.status_code == 412:
+                print('При дозагрузке файла был передан неверный диапазон в заголовке Content-Range')
+            elif response.status_code == 413:
+                print('Размер файла больше допустимого.\n' \
+                   'Если у вас есть подписка на Яндекс 360, можно загружать файлы размером до 50 ГБ,\n' \
+                   'если подписки нет — до 1 ГБ.')
+            elif response.status_code == 507:
+                print('Для загрузки файла не хватает места на Диске пользователя.')
+        return response.status_code
+
+
+if __name__ == '__main__':
+    client = VKClient(TOKEN_VK, "9214619")
+    result = client.get_photos()
+    
+    FILES_NAME = result
+    file_path = os.path.join(BASE_DIR, FILES_NAME)
+    
+    uploader = YandexDisk(TOKEN_Ya)
+    uploader.uploader(FILES_NAME, file_path)
 
